2565416383c04ab35fd20e80d6b7ffbd459eabf5
Rufino Rojas <lucras015@hotmail.com>
2019-05-14 14:48:00 -0500

    se agregaron page y servicio de peliculas

---
+++ src/app/detalle-peliculas/detalle-peliculas.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-peliculas',
+  templateUrl: './detalle-peliculas.page.html',
+  styleUrls: ['./detalle-peliculas.page.scss'],
+})
+export class DetallePeliculasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
